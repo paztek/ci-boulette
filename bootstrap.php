9231495067d3cb7e65a91e064a6d25860a0a287f
DamnDam <gnieofmars@gmail.com>
2013-04-18 01:44:54 +0200

    Favico + Clean bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -2,27 +2,33 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 
-use Symfony\Component\Yaml\Parser;
-
 $app = new Silex\Application();
 
 $app['debug'] = true;
 
-// The Logging Service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-	'monolog.logfile' => __DIR__.'/logs/silex.log',
-));
-
-$yaml = new Parser();
-
+// Parse users.yml
+$yaml = new Symfony\Component\Yaml\Parser();
 $userConfig = $yaml->parse(file_get_contents(__DIR__.'/users.yml'));
-
 $users = array();
-
 foreach ($userConfig as $username => $infos)
 {
     $users[$username] = array($infos['role'], $infos['password']);
 }
+
+// The Security service
+$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+        'security.firewalls' => array(
+                'login' => array(
+                        'pattern' => '/login$'),
+                'main' => array(
+                        'pattern' => '^/',
+                        'form' => array('login_path' => '/login', 'check_path' => '/login_check'),
+                        'logout' => array('logout_path' => '/logout'),
+                        'users' => $users
+                        )
+                )
+        )
+);
 
 // Doctrine DBAL Service provider
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
@@ -49,20 +55,10 @@ $app->register(new Dflydev\Silex\Provider\DoctrineOrm\DoctrineOrmServiceProvider
 // The Session service
 $app->register(new Silex\Provider\SessionServiceProvider());
 
-// The Security service
-$app->register(new Silex\Provider\SecurityServiceProvider(), array(
-        'security.firewalls' => array(
-                'login' => array(
-                        'pattern' => '/login$'),
-                'main' => array(
-                        'pattern' => '^/',
-                        'form' => array('login_path' => '/login', 'check_path' => '/login_check'),
-                        'logout' => array('logout_path' => '/logout'),
-                        'users' => $users
-                        )
-                )
-        )
-);
+// The Logging Service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+	'monolog.logfile' => __DIR__.'/logs/silex.log',
+));
 
 // The Twig service
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
