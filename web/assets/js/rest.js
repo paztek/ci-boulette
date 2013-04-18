e14637065fe5be02fb6f16972ca13f0014e6a60b
paztek <mbalmes@jolicode.com>
2013-04-18 16:14:13 +0200

    Fix bug in the slection of target <a /> tag in rest.js

--- web/assets/js/rest.js
+++ web/assets/js/rest.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('a[data-method]').on('click', function(event) {
 		event.preventDefault();
-		var a = $(event.target);
+		var a = $(event.target).closest('a');
 		var input = $('<input>').attr('type', 'hidden').attr('name', '_method').val(a.attr('data-method'));
 		$('<form />').attr('action', a.attr('href')).attr('method', 'POST').append(input).submit();
 	});
