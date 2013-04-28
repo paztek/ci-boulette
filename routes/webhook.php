9fb21eaed83f73ead0593458cdd19210fe19e6e7
Matthieu Balmes <mbalmes@jolicode.com>
2013-04-28 16:42:51 +0200

    Prevent creation of new Commit objects if commit hash already exists

--- routes/webhook.php
+++ routes/webhook.php
@@ -11,25 +11,25 @@ $webhookApp->post('/webhook', function(Request $request) use ($app) {
     $em = $app['orm.em'];
 
 	$app['monolog']->addDebug('[WEBHOOK]Receiving Hook.');
-	
+
     $payload = json_decode($request->request->get('payload'), true);
-	
+
 	// ob_start();
 	// var_dump($payload);
 	// $payload_dump = ob_get_contents();
 	// ob_end_clean();
 	// $app['monolog']->addDebug(sprintf("[WEBHOOK]%s", $payload_dump));
-	
+
     $repositoryUrl = $payload['repository']['url'];
-	
+
 	$app['monolog']->addInfo(sprintf("[WEBHOOK]Receiving hook from '%s'.", $repositoryUrl));
-	
+
     try {
         // We find the corresponding repository
         $repository = $em->getRepository('CiBoulette\Model\Repository')->findOneByUrl($repositoryUrl);
 
 		$app['monolog']->addInfo(sprintf("[WEBHOOK]Hook source is '%s'.", $repository));
-		
+
         // Push creation
         $push = new Push();
         $push->setRef($payload['ref']);
@@ -41,8 +41,12 @@ $webhookApp->post('/webhook', function(Request $request) use ($app) {
         $commitsData = $payload['commits'];
 
         // Creation of the commits
-        foreach ($payload['commits'] as $i => $commitData) {
-            $commit = new Commit();
+        foreach ($commitsData as $i => $commitData) {
+            try {
+                $commit = $em->getRepository('CiBoulette\Repository\Commit')->find($commitData['id']);
+            } catch (NoResultException $exception) {
+                $commit = new Commit();
+            }
             $commit->setHash($commitData['id']);
             $commit->setMessage($commitData['message']);
             $commit->setTimestamp(new \DateTime($commitData['timestamp']));
@@ -60,7 +64,7 @@ $webhookApp->post('/webhook', function(Request $request) use ($app) {
 
         // We try to find the commit referenced as 'before'. It may not exist.
         try {
-            $commit = $em->getRepository('CiBoulette\Model\Commit')->findOneByHash($payload['before']);
+            $commit = $em->getRepository('CiBoulette\Model\Commit')->find($payload['before']);
             $push->setBefore($commit);
         } catch (Exception $exception) {
             // Nothing to do
