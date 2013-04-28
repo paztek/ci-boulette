2e07e4e8386602f59a22bbf2745bbf9b0a4e12fb
Matthieu Balmes <mbalmes@jolicode.com>
2013-04-28 21:21:09 +0200

    CS Fixing

--- src/CiBoulette/Repository/PushRepository.php
+++ src/CiBoulette/Repository/PushRepository.php
@@ -9,12 +9,12 @@ class PushRepository extends BaseRepository
     public function findAllOrdered()
     {
         $builder = $this->createQueryBuilder('p')
-			->select('p, r, c, af, be')
-			->leftJoin('p.repository', 'r')
-			->leftJoin('p.commits', 'c')
-			->leftJoin('p.before', 'be')
-			->leftJoin('p.after', 'af')
-			->addOrderBy('p.timestamp', 'DESC');
+            ->select('p, r, c, af, be')
+            ->leftJoin('p.repository', 'r')
+            ->leftJoin('p.commits', 'c')
+            ->leftJoin('p.before', 'be')
+            ->leftJoin('p.after', 'af')
+            ->addOrderBy('p.timestamp', 'DESC');
 
         $query = $builder->getQuery();
 
