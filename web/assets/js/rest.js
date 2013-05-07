3f0b046032a0d1bbd3319353fbe379f98bfbaf29
Matthieu Balmes <mbalmes@jolicode.com>
2013-05-07 21:20:16 +0200

    Try to fix JS for Firefox and IE

--- web/assets/js/rest.js
+++ web/assets/js/rest.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 		event.preventDefault();
 		var a = $(event.target).closest('a');
 		var input = $('<input>').attr('type', 'hidden').attr('name', '_method').val(a.attr('data-method'));
-		$('<form />').attr('action', a.attr('href')).attr('method', 'POST').append(input).submit();
+		var form = $('<form />').attr('action', a.attr('href')).attr('method', 'POST').append(input);
+		$('body').append(form);
+		form.submit();
 	});
 });
